feb9c2a2926112a3bf37998c90c297677b58f070
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-27 13:38:15 +0200

    Display posts

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,27 @@ import path from 'path';
 // fs is a library exclusive to Node, so you can't use it directly in the browser. But if you import it at the top of the file and use it in a function that only runs in the server, then Next will remove this import from the client bundle
 import matter from 'gray-matter';
 
+import Link from 'next/link';
 import Layout from '../components/Layout';
+import Post from '../components/Post';
 
 export default function HomePage({ posts }) {
   console.log(posts);
 
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Posts</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+      <Link href='/blog'>
+        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
 }
